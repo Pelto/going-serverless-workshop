a1fbfcfcd4b0f62ff5d1dfd178ccf9b9903d26ab
Mattias Severson <mattias.severson@jayway.com>
2017-09-26 21:08:15 +0200

    get-game: added CORS and response tests.

--- lambdas/get-game/index.js
+++ lambdas/get-game/index.js
@@ -1,30 +1,35 @@
 'use strict';
 
-
 const parseEvent = require('./parse-event');
 const getGame = require('./get-game');
+const response = require('./response');
 
-
-function success(body) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(body)
-    };
-}
-
-const error = {
-    statusCode: 500
-};
 
 exports.handler = function(event, context, callback) {
-    const {gameId} = parseEvent(event);
-    getGame(gameId)
-        .then(gameState => {
-            const response = success(gameState);
-            callback(null, response);
-        })
-        .catch(err => {
-            console.error(JSON.stringify(err));
-            callback(null, error);
-        });
+
+    switch (event.httpMethod) {
+
+        case 'GET': {
+            const {gameId} = parseEvent(event);
+            return getGame(gameId)
+                .then(gameState => response.ok(gameState))
+                .then(resp => callback(null, resp))
+                .catch(err => {
+                    console.error(JSON.stringify(err));
+                    const resp = response.internalServerError();
+                    return callback(null, resp);
+                });
+        }
+
+        case 'OPTIONS': {
+            const resp = response.ok();
+            return callback(null, resp);
+        }
+
+        default: {
+            console.error(`Method not allowed: ${event.httpMethod}`);
+            const resp = response.methodNotAllowed();
+            return callback(null, resp);
+        }
+    }
 };
---
+++ lambdas/get-game/test/response-test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+process.env.STATIC_WEBSITE_URL = 'https://api.example.com';
+
+const response = require('../response');
+const chai = require('chai');
+const { expect } = chai;
+
+
+describe('Get Game Responses', () => {
+
+    it('200 OK', () => {
+        const responseBody = {key: 'value'};
+        const {statusCode, body} = response.ok(responseBody);
+
+        expect(statusCode).to.eql(200);
+        expect(body).to.eql(JSON.stringify(responseBody));
+    });
+
+
+    it('405 METHOD NOT ALLOWED', () => {
+        const {statusCode} = response.methodNotAllowed();
+
+        expect(statusCode).to.eql(405);
+    });
+
+
+    it('500 INTERNAL SERVER ERROR', () => {
+        const {statusCode} = response.internalServerError();
+
+        expect(statusCode).to.eql(500);
+    });
+
+
+    it('CORS headers', () => {
+        const {headers} = response.ok();
+
+        expect(headers).to.eql({
+            'Access-Control-Allow-Origin': 'https://api.example.com',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': 86400
+        });
+    })
+
+});
