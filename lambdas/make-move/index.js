4a77843ca9435668ae5747fb6e818e7e314bf65f
Mattias Severson <mattias.severson@jayway.com>
2017-09-26 21:47:36 +0200

    make-move: implemented CORS and added response tests.

--- lambdas/make-move/index.js
+++ lambdas/make-move/index.js
@@ -3,30 +3,36 @@
 const parseEvent = require('./parse-event');
 const gameRepo = require('./game-repo');
 const gameEngine = require('./game-engine');
+const response = require('./response');
 
-
-function success(body) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(body)
-    };
-}
-
-const error = {
-    statusCode: 500
-};
 
 exports.handler = function(event, context, callback) {
-    const {gameId, playerId, move} = parseEvent(event);
-    gameRepo.getGame(gameId)
-        .then(gameState => gameEngine.makeMove(gameState, playerId, move))
-        .then(gameRepo.saveGame)
-        .then(newGameState => {
-            const response = success(newGameState);
-            callback(null, response);
-        })
-        .catch(err => {
-            console.error(JSON.stringify(err));
-            callback(null, error);
-        });
+
+    switch (event.httpMethod) {
+
+        case 'POST': {
+            const {gameId, playerId, move} = parseEvent(event);
+            return gameRepo.getGame(gameId)
+                .then(gameState => gameEngine.makeMove(gameState, playerId, move))
+                .then(gameRepo.saveGame)
+                .then(newGameState => response.ok(newGameState))
+                .then(resp => callback(null, resp))
+                .catch(err => {
+                    console.error(JSON.stringify(err));
+                    const resp = response.internalServerError();
+                    callback(null, resp);
+                });
+        }
+
+        case 'OPTIONS': {
+            const resp = response.ok();
+            return callback(null, resp);
+        }
+
+        default: {
+            console.error(`Method not allowed: ${event.httpMethod}`);
+            const resp = response.methodNotAllowed();
+            return callback(null, resp);
+        }
+    }
 };
---
+++ lambdas/make-move/test/response-test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const chai = require('chai');
+const { expect } = chai;
+
+let response;
+
+describe('Make Move Responses', () => {
+
+    before(() => {
+        process.env.STATIC_WEBSITE_URL = 'https://api.example.com';
+        response = require('../response');
+    });
+
+
+    it('200 OK', () => {
+        const responseBody = {key: 'value'};
+        const {statusCode, body} = response.ok(responseBody);
+
+        expect(statusCode).to.eql(200);
+        expect(body).to.eql(JSON.stringify(responseBody));
+    });
+
+
+    it('405 METHOD NOT ALLOWED', () => {
+        const {statusCode} = response.methodNotAllowed();
+
+        expect(statusCode).to.eql(405);
+    });
+
+
+    it('500 INTERNAL SERVER ERROR', () => {
+        const {statusCode} = response.internalServerError();
+
+        expect(statusCode).to.eql(500);
+    });
+
+
+    it('CORS headers', () => {
+        const {headers} = response.ok();
+
+        expect(headers).to.eql({
+            'Access-Control-Allow-Origin': 'https://api.example.com',
+            'Access-Control-Allow-Methods': 'POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': 86400
+        });
+    })
+
+});
