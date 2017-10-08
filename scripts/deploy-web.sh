7783a42c62f7ac5a2936a21a191bfe2bb1b3938a
Johannes Pelto-Piri <johannes.peltopiri@jayway.com>
2017-10-08 20:11:17 +0200

    scripts: initial deploy-web script

---
+++ scripts/deploy-web.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -e
+
+script=$(basename $0)
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rootDir="$( cd "$scriptDir/.." && pwd )"
+region="eu-west-1"
+
+usage="usage: $script [-s|--stack-name -r|--region -h|--help]
+    -h| --help          this help
+    -r| --region        AWS region (defaults to '$region')
+    -s| --stack-name    stack name"
+
+
+#
+# For Bash parsing explanation, please see https://stackoverflow.com/a/14203146
+#
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -h|--help)
+        echo "$usage"
+        exit 0
+        ;;
+        -r|--region)
+        region="$2"
+        shift
+        ;;
+        -s|--stack-name)
+        stackName="$2"
+        shift
+        ;;
+        *)
+        # Unknown option
+        ;;
+    esac
+    shift # past argument or value
+done
+
+bucketName=(`aws cloudformation describe-stacks --stack-name $stackName \
+    --query "Outputs[?OutputKey == 'WebBucket'].OutputValue" \
+    --region $region \
+    --output text`)
+
+cd rps-client/
+
+npm install
+ng build --prod
+
+aws s3 sync dist/ s3://$bucketName/ --acl public-read
+
+
+
