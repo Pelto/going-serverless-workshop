1bb74dcce1f6a2ec9376a4012736e37db0be87be
Johannes Pelto-Piri <johannes.peltopiri@jayway.com>
2017-10-08 20:08:33 +0200

    client: started on dashboard

--- rps-client/src/app/dashboard.component.ts
+++ rps-client/src/app/dashboard.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'dashboard-component',
-    template: `
-        <h1>Welcome</h1>
-        <p>Lab instructions, use menu to continue</p>
-    `
+    templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
 
