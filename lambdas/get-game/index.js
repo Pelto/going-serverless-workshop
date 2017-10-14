7e4985cf5215c4931e8773d0ddba58ecc7165ff2
Mattias Severson <mattias.severson@jayway.com>
2017-10-14 19:26:11 +0200

    get-game: simplified code.

--- lambdas/get-game/index.js
+++ lambdas/get-game/index.js
@@ -1,22 +1,54 @@
 'use strict';
 
-const getGame = require('./get-game');
-const response = require('./response');
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION
+});
+
 
 function getGameId(event) {
     return event.pathParameters.gameId;
 }
 
 
+function getGame(gameId) {
+    const params = {
+        TableName : process.env.GAME_TABLE,
+        Key: {
+            gameId: gameId
+        }
+    };
+    return documentClient
+        .get(params)
+        .promise()
+        .then(data => data.Item);
+}
+
+
+function createResponse(httpStatus, responseBody) {
+    const response = {
+        statusCode: httpStatus
+    };
+    if (responseBody) {
+        response.body = JSON.stringify(body);
+    }
+    return response;
+}
+
+
 exports.handler = function(event, context, callback) {
 
     const gameId = getGameId(event);
+
     return getGame(gameId)
-        .then(gameState => response.ok(gameState))
-        .then(resp => callback(null, resp))
+        .then(gameState => {
+            const resp = createResponse(200, gameState);
+            return callback(null, resp);
+        })
         .catch(err => {
             console.error(JSON.stringify(err));
-            const resp = response.internalServerError();
+            const resp = createResponse(500);
             return callback(null, resp);
         });
 
