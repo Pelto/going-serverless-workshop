160019840b232c86ad5d194e20924c37c750f830
Mattias Severson <mattias.severson@jayway.com>
2017-10-14 13:45:33 +0200

    get-leaderboard: simplified implementation and removed CORS.

--- lambdas/get-leaderboard/index.js
+++ lambdas/get-leaderboard/index.js
@@ -1,33 +1,42 @@
 'use strict';
 
-const getLeaderboard = require('./get-leaderboard');
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION
+});
+
 const response = require('./response');
+
+
+function getLeaderboard() {
+    const params = {
+        TableName: process.env.SCORE_TABLE,
+        IndexName: 'scoreIndex',
+        AttributesToGet: ['playerId', 'score'],
+        KeyConditions: {
+            gameTitle: {
+                ComparisonOperator: "EQ",
+                AttributeValueList: ["Rock Paper Scissors"]
+            }
+        },
+        ScanIndexForward: false
+    };
+    return documentClient
+        .query(params)
+        .promise()
+        .then(data => data.Items);
+}
 
 
 exports.handler = function(event, context, callback) {
 
-    switch (event.httpMethod) {
-
-        case 'GET': {
-            return getLeaderboard()
-                .then(leaderboard => response.ok(leaderboard))
-                .then(resp => callback(null, resp))
-                .catch(err => {
-                    console.error(JSON.stringify(err));
-                    const resp = response.internalServerError();
-                    callback(null, resp);
-                });
-        }
-
-        case 'OPTIONS': {
-            const resp = response.ok();
-            return callback(null, resp);
-        }
-
-        default: {
-            console.error(`Method not allowed: ${event.httpMethod}`);
-            const resp = response.methodNotAllowed();
-            return callback(null, resp);
-        }
-    }
+    return getLeaderboard()
+        .then(leaderboard => response.ok(leaderboard))
+        .then(resp => callback(null, resp))
+        .catch(err => {
+            console.error(JSON.stringify(err));
+            const resp = response.internalServerError();
+            callback(null, resp);
+        });
 };
