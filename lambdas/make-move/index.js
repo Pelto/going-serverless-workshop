75131780e8e2fe47a3c6edd336177558e083f0e9
Johannes Pelto-Piri <johannes.peltopiri@jayway.com>
2017-10-15 22:08:37 +0200

    make-move: simplify make move

--- lambdas/make-move/index.js
+++ lambdas/make-move/index.js
@@ -1,39 +1,103 @@
 'use strict';
 
-const gameRepo = require('./game-repo');
-const gameEngine = require('./game-engine');
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10',
+    region: process.env.AWS_REGION
+});
 
+const WINNING_COMBINATIONS = {
+    ROCK: 'SCISSORS',
+    PAPER: 'ROCK',
+    SCISSORS: 'PAPER'
+};
 
-function parseRequestBody(event) {
-    return JSON.parse(event.body);
-}
-
-
-function createResponse(httpStatus, responseBody) {
-    const response = {
-        statusCode: httpStatus
+function getGame(gameId) {
+    const params = {
+        TableName: process.env.GAME_TABLE,
+        Key: {
+            gameId: gameId
+        }
     };
-    if (responseBody) {
-        response.body = JSON.stringify(body);
-    }
-    return response;
+    return documentClient
+        .get(params)
+        .promise()
+        .then(data => data.Item);
 }
 
+function canMakeMove(gameState) {
+    if (gameState === 'WINNER' || gameState === 'DRAW') {
+        throw new Error(`Unable to make move once state equals ${gameState.state}`);
+    }
+    return gameState;
+}
 
-exports.handler = function(event, context, callback) {
+function addPlayer(playerId, move) {
+    return function (gameState) {
+        console.info(gameState);
+        if (!gameState.players) {
+            gameState.players = [];
+        }
+        const player = { playerId, move };
+        gameState.players.push(player);
+        return gameState;
+    }
+}
 
-    const {gameId, playerId, move} = parseRequestBody(event);
+function updateState(gameState) {
 
-    return gameRepo.getGame(gameId)
-        .then(gameState => gameEngine.makeMove(gameState, playerId, move))
-        .then(gameRepo.saveGame)
-        .then(newGameState => {
-            const resp = createResponse(200, newGameState);
-            callback(null, resp);
-        })
-        .catch(err => {
-            console.error(JSON.stringify(err));
-            const resp = createResponse(500);
-            callback(null, resp);
+    if (gameState.players.length === 1) {
+        gameState.state = 'FIRST_MOVE';
+        return gameState;
+    }
+
+    const playerA = gameState.players[0];
+    const playerB = gameState.players[1];
+
+    if (playerA.move === playerB.move) {
+        gameState.state = 'DRAW';
+        return gameState;
+    }
+
+    gameState.state = 'WINNER';
+    gameState.winner = (WINNING_COMBINATIONS[playerA.move] === playerB.move)
+        ? playerA.playerId
+        : playerB.playerId;
+
+    return gameState;
+}
+
+function saveGame(gameState) {
+    const params = {
+        TableName: process.env.GAME_TABLE,
+        Item: gameState
+    };
+    return documentClient
+        .put(params)
+        .promise()
+        .then(() => gameState);
+}
+
+exports.handler = function (event, context, callback) {
+
+    const { gameId, playerId, move } = JSON.parse(event.body);
+
+    return getGame(gameId)
+        .then(canMakeMove)
+        .then(addPlayer(playerId, move))
+        .then(updateState)
+        .then(saveGame)
+        .then(gameState => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(gameState)
+        }))
+        .catch(error => {
+            console.error(error);
+            callback(null, {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: error.message
+                })
+            })
         });
 };
